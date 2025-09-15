7e1b855783727cb4fc2525f285470d8f9d4d181a
krishna kushwah <krishna kushwah@kk>
2025-09-15 12:34:16 +0530

    first commit

---
+++ java_selenium/src/main/java/array/Basic_array.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Basic_array {
+
+	public static void main(String[] args) 
+	{
+		int arr[]= {12,25,98};
+		int arr1[]=arr;
+		arr[1]=35;
+		System.out.println(arr[1]);
+		System.out.println(arr1[1]);
+
+	}
+
+}
---
+++ java_selenium/src/main/java/java_selenium_demo/First_progam.java
@@ -0,0 +1,11 @@
+package java_selenium_demo;
+
+public class First_progam {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("this is irst proram");
+		
+	}
+
+}
